5cb466a32c9aa8b084549e8d5ca1d46fb7d89414
IronHED <peter.mcquillan@me.com>
2019-09-04 18:57:46 -0400

    Changed Brand Image
    Changed the Brand to a Bulb
    
    Co-Authored-By: topsid <topsid@users.noreply.github.com>

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -46,7 +46,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className='navbar bg-dark'>
       <h1>
         <Link to='/'>
-          <i className='fas fa-code' /> Wireframe
+          <i class="fad fa-lightbulb"></i> Wireframe
         </Link>
       </h1>
       {!loading && (
